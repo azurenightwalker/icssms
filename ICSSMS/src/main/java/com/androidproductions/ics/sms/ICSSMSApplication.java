72d435e802ed886323de0d7d04b1fad78b0b43a8
Scott Jones <azurenightwalker@gmail.com>
2013-08-31 16:27:23 +0100

    Memory handling improvements

--- ICSSMS/src/main/java/com/androidproductions/ics/sms/ICSSMSApplication.java
+++ ICSSMS/src/main/java/com/androidproductions/ics/sms/ICSSMSApplication.java
@@ -4,10 +4,20 @@ import android.app.Application;
 import com.androidproductions.ics.sms.data.ImageCache;
 import com.androidproductions.ics.sms.preferences.ConfigurationHelper;
 
-/**
- * Created by Scott on 31/08/13.
- */
 public class ICSSMSApplication extends Application {
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        ImageCache.initInstance(Constants.MIN_CACHE_SIZE,getApplicationContext());
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        if (level > TRIM_MEMORY_UI_HIDDEN)
+            ImageCache.initInstance(Constants.MIN_CACHE_SIZE,getApplicationContext());
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
